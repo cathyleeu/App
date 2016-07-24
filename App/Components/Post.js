a61635e9af4a9f3f882bb954eb5ca254faf02ae7
cathyleeu <leeyu823@gmail.com>
2016-07-24 23:43:11 +0900

    포스트 수정중 이미지 불러오기 오류 수정중

--- App/Components/Post.js
+++ App/Components/Post.js
@@ -3,34 +3,160 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
+  Image,
+  TextInput,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
+import Util from './utils';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      num: 0,
+      selected: [],
+    };
+  }
+  getSelectedImages(images, current) {
+    var num = images.length;
+
+    this.setState({
+      num: num,
+      selected: images,
+    });
+
+    console.log(current);
+    console.log(this.state.selected);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          for Post
-        </Text>
+        <View style={styles.iconContainer}>
+          <Image style={styles.icon} source={{uri:'icon'}}></Image>
+        </View>
+        <TextInput
+          ref="textarea"
+          style={styles.textArea}
+          multiline={true}
+          placeholder="너의 하루를 말해봐봐봡？"
+          selectionColor="#2aa2ef"
+          placeholderTextColor="#ced8de"
+          />
+        <View style={styles.funcContainer}>
+          <View style={styles.funcIconCon}>
+            <View style={styles.funcIcon}>
+              <Icon name="ios-navigate" size={23} color="#8899a5" />
+              <Icon name="ios-camera" size={23} color="#8899a5" />
+              <Icon name="ios-image" size={23} color="#8899a5" />
+            </View>
+            <View style={styles.funcBtn}>
+              <TouchableHighlight style={styles.btn}>
+                <Text style={styles.btnText}>Post</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
+  container:{
+    paddingTop:30,
+    height:Util.size.height,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  icon:{
+    width:30,
+    height:30,
+    borderRadius:5,
   },
+  iconContainer:{
+    paddingLeft:15,
+    paddingRight:15,
+    flexDirection:"row",
+    justifyContent:"space-between",
+  },
+  textArea:{
+    height:335,
+    padding:15,
+    fontSize:20
+  },
+  funcContainer:{
+   height:275,
+   width:375,
+   position:"absolute",
+   bottom:0,
+   left:0,
+   borderTopWidth:1,
+   borderTopColor:"#a0adb7"
+ },
+ funcIconCon:{
+   height:50,
+   alignItems:"center",
+   justifyContent:"space-between",
+   flexDirection:"row",
+   borderBottomWidth:1,
+   borderBottomColor:"#ccd6dd"
+ },
+ funcIcon:{
+   width:210,
+   flexDirection:"row",
+   justifyContent:"space-around"
+ },
+ funcBtn:{
+   width:110,
+   flexDirection:"row",
+   justifyContent:"space-around",
+   alignItems:"center",
+ },
+ btn:{
+   height:35,
+   width:60,
+   alignItems:"center",
+   justifyContent:"center",
+   borderRadius:6,
+   borderColor:"#ccd6dd",
+   borderWidth:1
+ },
+ activeBtn:{
+   height:35,
+   width:60,
+   alignItems:"center",
+   justifyContent:"center",
+   borderRadius:6,
+   backgroundColor:"#2aa2ef"
+ },
+ btnText:{
+   color:"#ccd6dd",
+   fontSize:14
+ },
+ activeBtnText:{
+   color:"#fff",
+   fontSize:14
+ },
+ imageGrid:{
+   flexDirection:"row",
+   flexWrap:"wrap"
+ },
+ imageIcon:{
+   width: Util.size.width/3,
+   height:113,
+   alignItems:"center",
+   justifyContent:"center",
+   borderRightColor:"#ddd",
+   borderBottomColor:"#ddd",
+   borderRightWidth:1,
+   borderBottomWidth:1
+ },
+ image:{
+   width: Util.size.width/3,
+   height:113,
+ },
 });
 
 module.exports = Post;
--- App/Components/utils.js
+++ App/Components/utils.js
@@ -10,24 +10,6 @@ const Util = {
   size: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height
-  },
-  post(url, data, callback) {
-    const fetchOptions = {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    };
-
-    fetch(url, fetchOptions)
-    .then((response) => {
-      return response.json()
-    })
-    .then((responseData) => {
-      callback(responseData);
-    });
   }
 };
 
