6564a2079bdeb7a1ba4dbf1469613fac33b42880
cathyleeu <leeyu823@gmail.com>
2016-07-30 23:59:16 +0900

    added menupanel

--- App/Components/App/index.js
+++ App/Components/App/index.js
@@ -5,17 +5,53 @@
  */
 
 import { View } from 'react-native';
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './styles';
 import GlobalNav from '../GlobalNav';
+import Drawer from 'react-native-drawer'
+import MenuPanel from '../MenuPanel';
 
 
 class App extends Component {
+	state={
+    drawerOpen: false,
+    drawerDisabled: false,
+  };
+	closeDrawer = () => {
+    this._drawer.close()
+  };
+  openDrawer = () => {
+    this._drawer.open()
+  };
 	render() {
 		return (
-			<View style={styles.appContainer}>
-				<GlobalNav />
-			</View>
+			<Drawer
+				ref={(ref) => this._drawer = ref}
+				type="overlay"
+				content={
+					<MenuPanel closeDrawer={this.closeDrawer} />
+				}
+				acceptDoubleTap
+				styles={{main: {shadowColor: '#000000', shadowOpacity: 0.3, shadowRadius: 15}}}
+				onOpen={() => {
+					console.log('onopen')
+					this.setState({drawerOpen: true})
+				}}
+				onClose={() => {
+					console.log('onclose')
+					this.setState({drawerOpen: false})
+				}}
+				tweenDuration={100}
+				panThreshold={0.08}
+				disabled={this.state.drawerDisabled}
+				openDrawerOffset={0.2}
+				panOpenMask={0.2}
+				negotiatePan
+        >
+				<View style={styles.appContainer}>
+					<GlobalNav />
+				</View>
+      </Drawer>
 		);
 	}
 }
